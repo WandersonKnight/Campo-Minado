0aa25bee88dd7f7088fe4e2887b026bcc8ec4ae6
Wanderson Knight <39245594+WandersonKnight@users.noreply.github.com>
2022-05-22 15:09:00 -0300

    Delete src directory

--- src/Util.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Util {
-
-    public static void clearScreen() throws IOException, InterruptedException{
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-    }
-
-}
